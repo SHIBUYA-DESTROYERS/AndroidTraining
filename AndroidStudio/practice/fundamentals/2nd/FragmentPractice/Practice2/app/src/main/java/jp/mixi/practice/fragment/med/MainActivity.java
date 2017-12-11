92cf2f81bb0ce56dd730cc3ad2b4868cb22a802e
raryosu <me@raryosu.info>
2017-12-11 20:24:38 +0900

    んー？

--- AndroidStudio/practice/fundamentals/2nd/FragmentPractice/Practice2/app/src/main/java/jp/mixi/practice/fragment/med/MainActivity.java
+++ AndroidStudio/practice/fundamentals/2nd/FragmentPractice/Practice2/app/src/main/java/jp/mixi/practice/fragment/med/MainActivity.java
@@ -3,21 +3,21 @@ package jp.mixi.practice.fragment.med;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        // FragmentManager も、android.support.v4.app.FragmentManager を利用する
+        FragmentManager manager = getSupportFragmentManager();
+        // レイアウトから Fragment を見つけ出してインスタンスを得る
+        Fragment fragment = manager.findFragmentById(R.id.MyFragment);
     }
-
 }
+
