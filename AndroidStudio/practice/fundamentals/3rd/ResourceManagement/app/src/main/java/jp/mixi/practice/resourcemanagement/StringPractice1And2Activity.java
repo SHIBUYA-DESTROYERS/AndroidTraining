a5bef534393e196e5c3e96217ac835b0c9cb9949
raryosu <me@raryosu.info>
2017-12-17 15:47:19 +0900

    String Resource

--- AndroidStudio/practice/fundamentals/3rd/ResourceManagement/app/src/main/java/jp/mixi/practice/resourcemanagement/StringPractice1And2Activity.java
+++ AndroidStudio/practice/fundamentals/3rd/ResourceManagement/app/src/main/java/jp/mixi/practice/resourcemanagement/StringPractice1And2Activity.java
@@ -2,6 +2,7 @@ package jp.mixi.practice.resourcemanagement;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
 
 public class StringPractice1And2Activity extends ActionBarActivity {
 
@@ -9,6 +10,9 @@ public class StringPractice1And2Activity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.string_practice1);
+        String engineer = getString(R.string.engineer, "ほげ", "ふが", "にゃーん");
+        TextView text = (TextView)findViewById(R.id.textView6);
+        text.setText(engineer);
     }
 
 }
